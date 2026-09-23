047e56a1c2fb79abe01f098ff985ed3134cec65e
RabeyaMuna <rabeyakhatun.muna@mail.concordia.ca>
2026-09-23 16:41:46 -0400

    xuyisen__diff__id_46

--- src/accelerate/big_modeling.py
+++ src/accelerate/big_modeling.py
@@ -74,6 +74,7 @@ def init_empty_weights(include_buffers: bool = None):
     Any model created under this context manager has no weights. As such you can't do something like
     `model.to(some_device)` with it. To load weights inside your empty model, see [`load_checkpoint_and_dispatch`].
     Make sure to overwrite the default device_map param, otherwise dispatch is not called.
+
     </Tip>
     """
     if include_buffers is None:
